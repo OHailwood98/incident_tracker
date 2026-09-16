f10837ae582c6d4dbb7e464af2f918bb9e81c7ee
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2026-09-16 15:16:28 +0100

    add user to root reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
 import incidents from "./incidentReducers";
+import user from "./userReducer";
 
 const rootReducer = combineReducers({
+  user: user,
   incidents: incidents,
 });
 
