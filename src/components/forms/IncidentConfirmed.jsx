62a89ed58bddb5828a3ba79b557fc2d8a94c2424
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2026-09-17 12:02:21 +0100

    add form to show incident was added successfully

--- src/components/forms/IncidentConfirmed.jsx
+++ src/components/forms/IncidentConfirmed.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
 
-const IncidentConfirmed = ({ incidentID }) => {
+const IncidentConfirmed = ({ incidentID, setSuccess }) => {
   return (
     <div>
       <h5>Incident Saved</h5>
@@ -8,13 +9,16 @@ const IncidentConfirmed = ({ incidentID }) => {
         Your incident has been added to our system and will be investigated
         soon. The reference number for your incident is:
       </p>
-      <p>{incidentID}</p>
+      <h3>{incidentID}</h3>
+      <p>click here to be able to report more incidents</p>
+      <Button onClick={() => setSuccess(false)}>return</Button>
     </div>
   );
 };
 
 IncidentConfirmed.propTypes = {
   incidentID: PropTypes.string.isRequired,
+  setSuccess: PropTypes.func.isRequired,
 };
 
 export default IncidentConfirmed;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,23 +1,37 @@
-import IncidentForm from "../forms/IncidentForm";
 import api from "../../api";
 import { useState } from "react";
 
+import IncidentForm from "../forms/IncidentForm";
+import IncidentConfirmed from "../forms/IncidentConfirmed";
+
 export default function HomePage() {
   const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
+  const [incidnetID, setIncidnetID] = useState("");
 
   function submit(data) {
     console.dir(data);
-    api.incidents.addIncident(data).catch((err) => {
-      return setErrors(err.response.data.errors);
-    });
+    api.incidents
+      .addIncident(data)
+      .then((id) => {
+        console.dir(id);
+        setSuccess(true);
+        setIncidnetID(id);
+      })
+      .catch((err) => {
+        return setErrors(err.response.data.errors);
+      });
   }
   return (
     <div>
-      <h1 class="display-4">Add your Incident Here</h1>
+      <h1 class="display-4">Report your Incident Here</h1>
       <br />
       <hr />
       <br />
-      <IncidentForm submit={submit} errors={errors} />
+      {!success && <IncidentForm submit={submit} errors={errors} />}
+      {success && (
+        <IncidentConfirmed incidentID={incidnetID} setSuccess={setSuccess} />
+      )}
     </div>
   );
 }
